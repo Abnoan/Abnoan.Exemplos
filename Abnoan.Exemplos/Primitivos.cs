0617564eb47ae9ae90c3083e3220a39aeaa5d34a
Abnoan Muniz <abnoanmuniz@gmail.com>
2024-04-10 14:14:34 -0300

    commit inicial

---
+++ Abnoan.Exemplos/Primitivos.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Abnoan.Exemplos
+{
+    public class Primitivos
+    {
+        public int Inteiro = 0;
+        public long Longo = 0L;
+        public byte Byte = 0;
+        public short Curto = 0;
+        public uint UInteiro = 0U;
+        public ulong Ulongo = 0UL;
+        public sbyte SByte = 0;
+        public ushort UCurto = 0;
+        public float Flutuante = 0.0F;
+        public double Duplo = 0.0D;
+        public decimal Decimal = 0.0M;
+        public bool Booleano = true;
+        public char Caractere = 'A';
+        public string String = "Exemplo";
+    }
+}
